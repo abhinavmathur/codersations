def1faa15d97b53fed0e277d2407b14d7ca1d1ed
Abhinav Mathur <raaaaj5000@yahoo.co.in>
2016-03-30 01:11:28 -0400

    test ssh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,5 @@ class ApplicationController < ActionController::Base
   flash[:error] = t "#{policy_name}.#{exception.query}", scope: 'pundit', default: :default
   redirect_to(request.referrer || root_path)
   end
+  #hello
 end
